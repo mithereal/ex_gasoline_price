544ee9d3e7b5377e402a1c164cdf0d74bbcd30a3
Jason Clark <mithereal@gmail.com>
2018-05-11 10:52:32 -0700

    sync with hex

--- mix.exs
+++ mix.exs
@@ -7,7 +7,13 @@ defmodule Gasrate.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      build_embedded: Mix.env == :prod,
+      description: description(),
+      package: package(),
+      elixir: "~> 1.0",
+      name: "gasrate",
+      source_url: "https://github.com/mithereal/elixir-gasrate"
     ]
   end
 
@@ -24,7 +30,22 @@ defmodule Gasrate.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       {:floki, "~> 0.20.0"},
-      {:httpotion, "~> 3.1.0"}
+      {:httpotion, "~> 3.1.0"},
+      {:ex_doc, "~> 0.14", only: :dev}
+    ]
+  end
+  
+  defp description() do
+    "Fetch Gas Rates from gasprices.aaa.com."
+  end
+
+  defp package() do
+    [
+      name: "gasrate",
+      files: ["lib",  "mix.exs", "README*"],
+      maintainers: ["Jason Clark"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/mithereal/elixir-gasrate"}
     ]
   end
 end
